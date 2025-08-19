04804fd00ea6d71640fcf9519731484a06b9ef09
Nandani92 <nandanigoyani92@gmail.com>
2025-08-19 17:57:00 +0530

    Add files via upload

---
+++ Untitled6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,j,c,d,e,f;
+ printf("enter the two no: ");
+ scanf("%d %d",&i,&j);
+c=i+j;
+d=i-j;
+e=i*j;
+f=i/j;
+ printf("the add of two no = %d/n",c);
+ printf("the sub of two no = %d/n",d);
+ printf("the multiply of two no = %d/n",e);
+ printf("the division of two no = %d/n",f);
+return 0;
+}
